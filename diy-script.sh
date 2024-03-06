eae9c49dff80ac40da6356acabd1378dfae1fdb3
Tan Feng <wstanfeng@gmail.com>
2024-03-06 20:14:00 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -21,7 +21,7 @@ rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/packages/multimedia/aliyundrive-webdav
 
 # 移除 openwrt feeds 自带的核心包
-# rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 
 # 更新go
 # rm -rf feeds/packages/lang/golang
