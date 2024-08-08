ceb6f397834ac694da92dd417fda87281f607b3c
Tan Feng <wstanfeng@gmail.com>
2024-08-08 13:19:26 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -66,7 +66,7 @@ git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/
 
 # 科学上网插件
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/small
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/helloworld
+# git clone --depth=1 -b main https://github.com/fw876/helloworld package/helloworld
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
